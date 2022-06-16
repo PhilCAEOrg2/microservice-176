2e4cb3a7af6c703536a9cc73774753a52d04b111
PhilCAEUser <mail@rwth>
2022-06-16 10:07:58 +0000

    update name abcd

--- app/src/main/java/i5/las2peer/services/res/Res.java
+++ app/src/main/java/i5/las2peer/services/res/Res.java
@@ -71,7 +71,7 @@ public class Res extends RESTService {
 
   @Api
   @SwaggerDefinition(
-      info = @Info(title = "176", version = "0.0.0",
+      info = @Info(title = "176", version = "",
           description = "",
           termsOfService = "",
           contact = @Contact(name = "", email = "CAEAddress@gmail.com") ,
